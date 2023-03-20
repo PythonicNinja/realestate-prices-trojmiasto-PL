65090b6748ef6921ce30c9a67785b14571fc702e
Wojtek Nowak <mail@pythonic.ninja>
2023-03-20 07:18:03 +0100

    new code in analyse,

--- analyse.py
+++ analyse.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('/Users/pythonicninja/PycharmProjects/realestate-prices-trojmiasto-PL/cached_df.csv')
+df = df.sort_values('gain', ascending=False)
 print(df)
+
+top_10_locations = df.to_numpy()[:10]
+print(f"top_10_locations {top_10_locations}")
